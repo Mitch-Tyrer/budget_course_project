74796d816d11f744b3f84ab60e934b32b0fd6d7a
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-09 16:16:33 -0700

    trying Jonas' code

--- app.js
+++ app.js
@@ -27,26 +27,31 @@ var budgetController = (function () {
     };
 
     return {
-        addItem: function (type, des, val) {
+        addItem: function(type, des, val) {
             var newItem, ID;
-            //[1,2,3,4,5], next ID = 6
-            //[1,2,4,6,8], next ID =9
-            //ID = last ID + 1
-            //create newID
+            
+            //[1 2 3 4 5], next ID = 6
+            //[1 2 4 6 8], next ID = 9
+            // ID = last ID + 1
+            
+            // Create new ID
             if (data.allItems[type].length > 0) {
                 ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
             } else {
                 ID = 0;
             }
-            //create new item based on inc or exp type
+            
+            // Create new item based on 'inc' or 'exp' type
             if (type === 'exp') {
                 newItem = new Expense(ID, des, val);
             } else if (type === 'inc') {
                 newItem = new Income(ID, des, val);
             }
-            //push it into datastructure
+            
+            // Push it into our data structure
             data.allItems[type].push(newItem);
-            //return the new element
+            
+            // Return the new element
             return newItem;
         },
         testing: function () {
@@ -66,9 +71,7 @@ var UIController = (function () {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputBtn: '.add__btn',
-        incomeContainer: '.income__list',
-        expensesContainer: '.expenses__list'
+        inputBtn: '.add__btn'
     }
 
     return {
