f389d7132c5d00ee2a8ddf6a1fe95c67a57e440c
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-09 14:31:10 -0700

    created initialization function and added budget constructors and a data structure

--- app.js
+++ app.js
@@ -2,8 +2,29 @@
 //Control Module
 //==================
 var budgetController = (function () {
+    //create a constructor function for inc and exp
+    var Expense = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var Income = function (id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var data = {
+        allItems: {
+            exp:[],
+            inc:[]
+        },
+        totals: {
+            exp: 0,
+            inc:0
+        }
+    }
 
 
 })();
