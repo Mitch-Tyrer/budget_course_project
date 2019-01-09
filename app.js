3ef9fbebb7b320ed0421f0015bb7119758515a53
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-08 20:08:25 -0700

    added ability to read data inputs from UI controller

--- app.js
+++ app.js
@@ -10,8 +10,28 @@ var budgetController = (function () {
 //==================
 //UI Module
 //==================
-var UIController = (function(){
+var UIController = (function () {
+    
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+    
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            }
+        },
 
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    }
 
 
 
@@ -20,11 +40,14 @@ var UIController = (function(){
 //==================
 //App Controller Module
 //==================
-var controller = (function(budgetCtrl, UICtrl) {
+var controller = (function (budgetCtrl, UICtrl) {
+
+    var DOM = UICtrl.getDOMstrings();
 
     var ctrlAddItem = function () {
         // 1. Get field input data
-
+        var input = UICtrl.getInput();
+        console.log(input);
         //2. add item to budget controller
 
         //3. add item to UI
@@ -33,13 +56,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 
         //5. display budger on UI
 
-        console.log('it works')
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (event) {
-        if(event.keycode === 13 || event.which === 13){
+        if (event.keycode === 13 || event.which === 13) {
             ctrlAddItem();
         }
     });
