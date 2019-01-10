c5446a5e35a8eb3e718d2f28b4efce9e1c13b881
Mitch-Tyrer <mltcinematography@gmail.com>
2019-01-09 17:18:36 -0700

    added budget calculation to budget controls

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ var budgetController = (function () {
         this.value = value;
     };
 
+    var calculateTotal = function(type){
+        var sum = 0;
+        data.allItems[type].forEach(function (current) {
+            sum += current.value;
+        });
+        data.total[type] = sum;
+    };
+
     var data = {
         allItems: {
             exp: [],
@@ -23,7 +31,9 @@ var budgetController = (function () {
         totals: {
             exp: 0,
             inc: 0
-        }
+        },
+        budget: 0,
+        percentage: -1
     };
 
     return {
@@ -49,6 +59,31 @@ var budgetController = (function () {
             //return the new element
             return newItem;
         },
+
+        calculateBudget: function () {
+            //calculate total income and expenses
+            calculateTotal('exp');
+            calculateTotal('inc');
+            //calculate the budget: income - expenses
+            data.budget = data.totals.inc - data.totals.exp
+            //calculate the percentage of income that was spent
+            if(data.totals.inc > 0){
+                data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+            } else {
+                data.percentage = -1;
+            }
+            
+        },
+
+        getBudget = function () {
+            return {
+                budget: data.budget,
+                totalInc: data.totals.inc,
+                totalExp: data.totals.exp,
+                percentage: data.percentage
+            }
+        },
+
         testing: function () {
             console.log(data);
         }
@@ -136,9 +171,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     var updateBudget = function () {
         //1. Calc budget
-
+        budgetCtrl.calculateBudget();
         // 2. return the budget
-
+        var budget = budgetCtrl.getBudget();
         //3. display the budget on UI
     }
 
